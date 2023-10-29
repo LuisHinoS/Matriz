6a6f13d9b7c198451f26dde4d7d1f3ee355bf137
LuisHinoS <129122338+LuisHinoS@users.noreply.github.com>
2023-10-29 15:28:55 -0300

    Create calculoMatriz.c

---
+++ calculoMatriz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int tamanho;
+    printf("Informe o tamanho da matriz quadrada: ");
+    scanf("%d", &tamanho);
+
+    int matriz[50][50];
+
+    printf("Insira os elementos da matriz:\n");
+    for (int i = 0; i < tamanho; i++) {
+        for (int j = 0; j < tamanho; j++) {
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    int somaPrincipal = 0;
+    int somaSecundaria = 0;
+
+    for (int i = 0; i < tamanho; i++) {
+        somaPrincipal += matriz[i][i];
+        somaSecundaria += matriz[i][tamanho - 1 - i];
+    }
+
+    printf("Soma da Diagonal Principal: %d\n", somaPrincipal);
+    printf("Soma da Diagonal Secundária: %d\n", somaSecundaria);
+
+    return 0;
+}
